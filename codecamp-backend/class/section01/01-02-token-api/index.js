f42013bf01f1bb30a9d1dbde798fa981457c10cd
Jinsol Kim <jinsol032360@gmail.com>
2023-12-28 15:47:55 +0900

    feat. 인증 토큰 만들기

---
+++ codecamp-backend/class/section01/01-02-token-api/index.js
@@ -0,0 +1,17 @@
+// API 만들기
+function createTokenOfPhone(phoneNum) {
+    // 1. 휴대폰번호 자릿수 맞는지 확인하기
+    // 휴대폰 번호의 길이가 10보다 작거나 11보다 크다면
+    if (phoneNum.length < 10 || phoneNum.length > 11) {
+        // 에러 메시지를 콘솔에 출력
+        console.log('핸드폰 번호를 제대로 입력해주세요!');
+        return;
+    }
+
+    // 2. 휴대폰 토큰 6자리 만들기
+
+    // 3. 휴대폰번호에 토큰 전송하기
+}
+
+// API 실행하기
+createTokenOfPhone('01031472691');
