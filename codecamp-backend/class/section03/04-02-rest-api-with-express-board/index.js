da49bde6e87ad5fe71464549bfbe548047a7ffb0
Jinsol Kim <jinsol032360@gmail.com>
2024-01-03 09:34:04 +0900

    게시글 조회 / 등록 API 실습

--- codecamp-backend/class/section03/04-02-rest-api-with-express-board/index.js
+++ codecamp-backend/class/section03/04-02-rest-api-with-express-board/index.js
@@ -6,7 +6,39 @@ const app = express();
 
 // GET요청이 들어왔을 때
 app.get('/boards', (req, res) => {
-    res.send('Hello World!');
+    // 1. 데이터를 조회하는 로직 => DB에 접속해서 데이터 꺼내오기
+    const result = [
+        {
+            number: 1,
+            writer: '철수',
+            title: '제목입니다~~',
+            contents: '내용이에요!!!',
+        },
+        {
+            number: 2,
+            writer: '영희',
+            title: '영희 제목입니다~~',
+            contents: '영희 내용이에요!!!',
+        },
+        {
+            number: 3,
+            writer: '훈이',
+            title: '훈이 제목입니다~~',
+            contents: '훈이 내용이에요!!!',
+        },
+    ];
+    // 2. 꺼내온 결과 응답 추가
+    res.send(result);
+});
+
+app.use(express.json());
+app.post('/boards', (req, res) => {
+    console.log(req.body);
+
+    // 1. 데이터를 등록하는 로직 => DB에 접속해서 데이터 저장하기
+
+    // 2. 저장 결과 응답 추가
+    res.send('게시물 등록에 성공하였습니다.');
 });
 
 app.listen(3000, () => {
