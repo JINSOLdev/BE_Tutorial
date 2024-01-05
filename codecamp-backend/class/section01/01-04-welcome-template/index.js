af86d0e4c9c46fc14b951c1ab30d30083a6abaa7
Jinsol Kim <jinsol032360@gmail.com>
2024-01-05 15:40:49 +0900

    :bulb: how to make email template

--- codecamp-backend/class/section01/01-04-welcome-template/index.js
+++ codecamp-backend/class/section01/01-04-welcome-template/index.js
@@ -1,23 +1,24 @@
-const name = 'amy';
-const age = 21;
-const school = 'Havard University';
-const createAt = '2023-12-28';
-
 function getWelcomeTemplate({ name, age, school, createAt }) {
-    const mytemplate = `
-  <html>
-  <body>
-      <h1>${name}님 가입을 환영합니다!!!</h1>
-      <hr />
-      <div>이름: ${name}</div>
-      <div>나이: ${age}</div>
-      <div>학교: ${school}</div>
-      <div>가입일: ${createAt}</div>
-  </body>
-</html>
+    const myTemplate = `
+                        <html>
+                          <body>
+                              <h1>${name}님 가입을 환영합니다!!!</h1>
+                              <hr />
+                              <div>이름: ${name}</div>
+                              <div>나이: ${age}</div>
+                              <div>학교: ${school}</div>
+                              <div>가입일: ${createAt}</div>
+                          </body>
+                        </html>
   
   `;
-    console.log(mytemplate);
+    console.log(myTemplate);
 }
 
+const name = 'amy';
+const age = 21;
+const school = 'Harvard University';
+const createAt = '2023-12-28';
+
+// 중괄호를 사용하여 인자를 나열하는 방법
 getWelcomeTemplate({ name, age, school, createAt });
