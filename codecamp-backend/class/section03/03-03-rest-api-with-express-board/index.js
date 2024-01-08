57017405d27c25887805c3cee8d77fc2c4a3d1ed
Jinsol Kim <jinsol032360@gmail.com>
2024-01-08 14:22:11 +0900

    :bulb: how to read json in express

---
+++ codecamp-backend/class/section03/03-03-rest-api-with-express-board/index.js
@@ -0,0 +1,37 @@
+// const express = require('express') => 옛날방식 commonjs, 요즘방식 module
+import express from 'express';
+
+const app = express();
+
+// express에서 json 읽는 방법
+app.use(express.json());
+
+// GET 요청이 들어왔을 때
+app.get('/boards', (req, res) => {
+    // 1. DB에 접속 후, 데이터를 조회 => 데이터를 조회했따고 가정
+    const result = [
+        { number: 1, writer: '철수', title: '제목입니다~', content: '내용이에요' },
+        { number: 2, writer: '영희', title: '영희입니다~', content: '영희이에요' },
+        { number: 3, writer: '훈이', title: '훈이입니다~', content: '훈이이에요' },
+    ];
+
+    // 2. DB에서 꺼내온 결과를 브라우저에 응답(response)주기
+    res.send(result);
+});
+
+app.post('/boards', (req, res) => {
+    // 1. 브라우저에서 보내준 데이터 확인하기
+    console.log(req);
+    console.log('============');
+    console.log(req.body);
+
+    // 2. DB에 접속 후, 데이터를 저장 => 데이터 저장했다고 가정
+
+    // 3. DB에 저장된 결과를 브라우저에 응답(response)주기
+    res.send('게시물 등록에 성공했습니다.');
+});
+
+// 3000번 포트에서 실행
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`);
+});
